9d02efa4ae2d7413269b3ea2c8cf4ba5dbe4e191
Tim Hintz <tjhintz@gmail.com>
2020-09-26 22:41:08 -0400

    Archimedes method for approximating pi by walking around a unit circle

---
+++ in_his_steps.py
@@ -0,0 +1,19 @@
+import math
+
+def in_his_footsteps(steps):
+    """
+    A math function that approximates pie using hypothetical even steps aropund
+    a unit circle, similar to how Archimedes stumpled upon pi by walking around
+    a circle of known area in 250 bce
+
+
+    """
+    radius = 1 # unit circle
+    theta = 2*math.pi/steps #internal angle of each triangle at center is 2pi/steps
+    half_theta = theta/2 # split isosceles into two right angles triangles
+    hypotenuse = radius # Hypotenuse is the radius in this case
+    half_opposite = math.sin(half_theta) * hypotenuse # opposite is h*sin(theta)
+    triangle_perimeter = half_opposite*2*steps # total perimeter length of all triangles
+    pi_approx = triangle_perimeter/2*radius
+    pi_diff = round(math.pi - pi_approx,3)
+    return "{} even steps around a circle would have gotten Archimedes within {} of pi".format(steps, pi_diff)
